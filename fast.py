f1c3a34dddbed04d2984aac11f036cdf58157b67
jihad <jihad_ashor@yahoo.com>
2024-09-04 01:22:22 +0300

    pll

---
+++ fast.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+
+
+
+)
+
+@app.get("/")
+def Hi():
+    return {"message": "Hello World"}
+
+@app.get("/bader")
+def bader_washah(x: str , y : str , z: str):
+
+    if x=="jihad" and y =="love" and z=="bader":
+        p = "U GOT IT"
+
+        return {"**" : p}
+    else:
+        return {"message": "NOOOO"}
